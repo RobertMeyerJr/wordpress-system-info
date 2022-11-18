32dc75ab3cc2f377fd3115c2aec07ea578ee7659
Robert Meyer Jr <RobertMeyerJr@gmail.com>
2022-11-18 17:22:50 -0500

    Sessions

--- views/admin/tabs/Sessions.php
+++ views/admin/tabs/Sessions.php
@@ -1,4 +1,4 @@
-
+<?php if ( !defined('ABSPATH') ){ die('-1'); } ?>
 <?php
 global $wpdb;
 $sql = "SELECT u.user_email,u.user_nicename, m.meta_value 
@@ -9,12 +9,14 @@ AND meta_value != ''
 LIMIT 200";
 
 $sessions = $wpdb->get_results($sql);
+
+#Todo: Make sure we are an admin before showing anything
 ?>
 <table class='wp-list-table widefat fixed striped'>
     <thead>
         <tr>
             <th>Email</th>
-            <th>Name</th>
+            <th>Username</th>
             <th>IP</th>
             <th>User Agent</th>
             <th>Login DateTime</th>
@@ -30,7 +32,14 @@ $sessions = $wpdb->get_results($sql);
             <td><?=esc_html($s->user_nicename)?></td>
             <td><?php echo $last['ip'] ?? '' ?></td>
             <td><?php echo $last['ua'] ?? '' ?></td>
-            <td><?php echo date('n/j/Y g:ia e', $last['login']); ?></td>
+            <td>
+                <?php echo date('n/j/Y g:ia e', $last['login']); ?>
+                <?php 
+                    if(!empty($last['login'])){
+                        echo '<div>'.human_time_diff($last['login']).' ago</div>';
+                    }
+                ?>
+            </td>
         </tr>
     <?php endforeach; ?>
     </tbody>

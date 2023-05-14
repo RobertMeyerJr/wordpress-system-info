591ce75da5181703436fd3dfbe40bc353f7aeec1
Robert Meyer Jr <RobertMeyerJr@gmail.com>
2023-05-14 10:27:49 -0400

    Blocks and Constants

---
+++ views/admin/tabs/blocks.php
@@ -0,0 +1,27 @@
+get registered blocks
+
+output them.
+Screenshot if we have.
+Indicate if Dynamic
+
+Also show theme.json settings?
+<?php
+
+$blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+#d($blocks);
+?>
+<table>
+<?php foreach($blocks as $b) : ?>
+    <tr>
+        <th><?=$b->name?></th>
+        <td><?=$b->title?></td>
+        <td><?=$b->category?></td>
+        <td><?=$b->description?></td>
+        <td><?=is_scalar($b->render_callback) ? $b->render_callback: gettype($b->render_callback);?>
+        <td><?=$b->provides_context?>
+        <td><?php echo (!empty($b->uses_context)) ? print_r($b->uses_context) : '' ?>
+        <td><?=$b->example?>
+    </tr>
+<?php endforeach; ?>
+</table>
---
+++ views/admin/tabs/constants.php
@@ -0,0 +1,21 @@
+<h5><label>Search</label> <input class=dbg_search><br/></h5>
+<style>
+.value-info.expanded{white-space: pre;}
+</style>
+<?php System_Info_Tools::dbg_table_out(get_defined_constants(true)); ?>
+<script>
+jQuery(function($){
+	$('.value-info').click(function(){ $(this).toggleClass('expanded') });
+
+	$('input.dbg_search').change(function(){
+		var v = $(this).val();
+		if(v.length){
+			$('table.dbg_out tr').hide();
+			$('table.dbg_out tr:contains('+v+')').show();
+		}
+		else{
+			$('table.dbg_out tr').show();
+		}
+	});
+});    
+</script>

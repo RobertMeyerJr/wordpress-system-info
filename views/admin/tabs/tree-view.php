cf5e85a064f58801432fc9f1d7e1e21be38f8e08
Robert Meyer Jr <RobertMeyerJr@users.noreply.github.com>
2024-02-03 11:37:18 -0500

    Tree View Updates

--- views/admin/tabs/tree-view.php
+++ views/admin/tabs/tree-view.php
@@ -32,7 +32,7 @@ ul.child-pages{
 }
 .dates{padding-right:40px;}
 
-.info{
+#page_tree .info{
     padding:5px;
     width:auto;
     max-width:40vw;
@@ -41,6 +41,7 @@ ul.child-pages{
     display:none;
     position:absolute;
     top:0;
+    left:200px;
     z-index:10;
     margin-left:30px;
 }
@@ -57,11 +58,16 @@ ul.child-pages{
     text-align:center;
 }
 #page_tree a{text-decoration:none;}
+
+#page_tree .optional{display:none;}
+#page_tree.show_created .created{display:inline-block;}
+#page_tree.show_modified .modified{display:inline-block;}
+#page_tree.show_template .template{display:inline-block;}
+#page_tree.show_author .author{display:inline-block;}
 </style>
 <?php if(!empty($sites)) : ?>
 <form action="<?php echo admin_url('admin.php')?>">
     <input type=hidden name=page value="wptd-tree-view">
-    
         <select name=blog_id>
             <?php foreach($sites as $s) : ?>
                 <option <?php selected($s->blog_id == $blog_id) ?> value="<?php echo esc_attr($s->blog_id)?>"><?php echo $s->domain?></option>
@@ -72,11 +78,13 @@ ul.child-pages{
 <?php endif; ?>
 <br/>
 <div id=options>
-    <input type=text placeholder="Search" id=search_value>
-    <label>Show Created Date<input type=checkbox id=show_created checked value=1></label>
-    <label>Show Modified Date<input type=checkbox id=show_modified value=1></label>
-    <label>Show Author<input type=checkbox id=show_author value=1></label>
-    <label>Show Template<input type=checkbox id=show_tpl checked value=1></label>
+    <div style="float:right">
+        <label>Created<input type=checkbox id=show_created value=show_created></label>
+        <label>Modified<input type=checkbox id=show_modified value=show_modified></label>
+        <label>Author<input type=checkbox id=show_author value=show_author></label>
+        <label>Template<input type=checkbox id=show_tpl value=show_template></label>
+    </div>
+    <input type=text placeholder="Search" id=search_value>        
     <select id=page_builder>
         <option value="">Any</option>
         <option value="js_composer">WP Bakery</option>
@@ -107,6 +115,11 @@ $pages = get_posts([
 <?php switch_to_blog($current_blog_id); #Switch Back?>
 <script>
 jQuery(function($){
+
+    $('#options input[type=checkbox]').click(function(){
+        $('#page_tree').toggleClass($(this).val());
+    });
+
     $('#tree_search').click(search);
     $('#page_tree li .expand').on('click',function(){
         console.log('Clicked Expand');
@@ -160,16 +173,51 @@ function child_tree($page, $pages, $level=0){
         <span title="<?=esc_attr($page->post_title)?>">
             <a target=_blank href="<?=$url?>">
                 <?=str_replace($base_url,'',$url)?>
-            </a>
-            <span class=cGreen>
-                <?=the_author();?>
-            </span>
-            
-            
+            </a>    
         </span>
         
-        <div class=info>
-            <h3><?=$page->post_title?></h3>            
+        <span class="page-builder pull-right">
+            <?php if($vc) : ?><span title="WP Bakery" class="js_composer"><?php include(WP_CONTENT_DIR .'/plugins/js_composer/assets/vc/logo/wpb-logo-white_20.svg');?></span><?php endif; ?>
+            <?php if($gb) : ?><span title="Gutenberg" class="gutenberg">G</span><?php endif; ?>
+            <?php if($divi) : ?><span title="Divi" class="divi">D</span><?php endif; ?>
+        </span>
+        <span class="optional cGreen author">
+            <?=get_user_by('id',$post->post_author)->display_name ?? '';?>
+        </span>
+        <span title="<?=esc_attr($tpl)?>" class="optional template pull-right text-muted">
+            <?=!empty($tpl) && $tpl != 'default' ? str_replace(['templates/','.php'],'',$tpl):''?>
+        </span>
+        <span class="dates cGreen pull-right">
+                <?php 
+                $created    = date('n/j/Y g:ia',  strtotime($post->post_date));
+                $modified   = date('n/j/Y g:ia', strtotime($post->post_modified));
+                $title = "Created {$created} Modified {$modified}";
+                ?>
+                <span title="<?=esc_attr($title)?>" class="text-green created optional">
+                    <?php echo date('n/j/Y',  strtotime($post->post_date))?>
+                </span>
+                <span title="<?=esc_attr($title)?>" class="text-yellow modified optional">
+                    <?php echo date('n/j/Y',  strtotime($post->post_modified))?>
+                </span>
+        </span>
+    
+    <?php if( !empty($h) ) : ?>
+        <ul>
+        <?php foreach($h as $child_id => $child_title) : ?>
+        <?php $child_page = get_post($child_id); ?>
+        <?php echo child_tree($child_page, $pages, $level+1); ?>
+        <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+    
+    <div class=info>
+            <h3><?=$page->post_title?></h3>
+            <div>
+            <a target=_blank href="<?=$url?>">
+                <?=str_replace($base_url,'',$url)?>
+            </a>
+            </div>
             <div class=cGreen>
                 Created <?=$post->post_date;?>
                 -  
@@ -185,36 +233,11 @@ function child_tree($page, $pages, $level=0){
             <?php endif; ?>
             <div class=actions>
                 <a href="<?php echo get_edit_post_link()?>">Edit</a>
+                <a  onclick="return confirm('Move this page to the trash? You can restore it later.');"
+                    style="color:red" href="<?php echo get_delete_post_link()?>">Delete</a>
             </div>
         </div>
 
-        <span class="page-builder pull-right">
-            <?php if($vc) : ?><span title="WP Bakery" class="js_composer"><?php include(WP_CONTENT_DIR .'/plugins/js_composer/assets/vc/logo/wpb-logo-white_20.svg');?></span><?php endif; ?>
-            <?php if($gb) : ?><span title="Gutenberg" class="gutenberg">G</span><?php endif; ?>
-            <?php if($divi) : ?><span title="Divi" class="divi">D</span><?php endif; ?>
-        </span>
-        <span title="<?=esc_attr($tpl)?>" class="template pull-right text-muted">
-            <?=!empty($tpl) && $tpl != 'default' ? str_replace(['templates/','.php'],'',$tpl):''?>
-        </span>
-        <span class="dates cGreen pull-right">
-                <?php 
-                $created    = date('n/j/Y g:ia',  strtotime($post->post_date));
-                $modified   = date('n/j/Y g:ia', strtotime($post->post_modified));
-                $title = "Created {$created} Modified {$modified}";
-                ?>
-                <span title="<?=esc_attr($title)?>" class="text-green">
-                    <?php echo date('n/j/Y',  strtotime($post->post_date))?>
-                </span>
-        </span>
-    
-    <?php if( !empty($h) ) : ?>
-        <ul>
-        <?php foreach($h as $child_id => $child_title) : ?>
-        <?php $child_page = get_post($child_id); ?>
-        <?php echo child_tree($child_page, $pages, $level+1); ?>
-        <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
     </li>
     <?php
     

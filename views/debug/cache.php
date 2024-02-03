bafe6447c705ff10871dff5b6715c126823862c6
Robert Meyer Jr <RobertMeyerJr@users.noreply.github.com>
2024-02-03 11:37:09 -0500

    Include Cache Hits

--- views/debug/cache.php
+++ views/debug/cache.php
@@ -15,3 +15,24 @@ global $wp_object_cache;
 		<tr><th></th><td><?php if(isset($wp_object_cache->misses)) 	echo number_format($wp_object_cache->misses) ?></td></tr>	
 	<?php endif; ?>
 </table>
+
+<?php if(!empty(System_Info::$cache_hits)) : ?>
+	<h2>Cache Hits <?=number_format(count(System_Info::$cache_hits))?></h2>
+	<table>
+		<?php foreach(System_Info::$cache_hits as $k=>$c) : ?>
+			<tr>
+				<th><?=$k?></th>
+				<td><?=$c?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+	<h2>Cache Miss <?=number_format(count(System_Info::$cache_misses))?></h2>
+	<table>
+		<?php foreach(System_Info::$cache_misses as $k=>$c) : ?>
+			<tr>
+				<th><?=$k?></th>
+				<td><?=$c?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+<?php endif; ?>
